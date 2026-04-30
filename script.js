787684cb6a13c0afd7a97b12d03e9aeb40e878f3
sqrt <nkcolasmax@gmail.com>
2026-04-29 22:38:38 -0400

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-console.log("Bienvenido al sistema de sqrt 🚀");
+const canvas = document.getElementById("matrix");
+const ctx = canvas.getContext("2d");
 
-const text = "Accediendo...";
-let i = 0;
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
-function typing() {
-  if (i < text.length) {
-    document.title = text.substring(0, i+1);
-    i++;
-    setTimeout(typing, 100);
+const letters = "01";
+const fontSize = 14;
+const columns = canvas.width / fontSize;
+
+const drops = Array(Math.floor(columns)).fill(1);
+
+function draw() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#0f0";
+  ctx.font = fontSize + "px monospace";
+
+  for (let i = 0; i < drops.length; i++) {
+    const text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+
+    drops[i]++;
   }
 }
 
-typing();
+setInterval(draw, 33);
